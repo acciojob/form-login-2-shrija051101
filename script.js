cf7e40ffef69e015aa8f711b508f795a228aae2a
Shrija Das <134855439+shrija051101@users.noreply.github.com>
2023-11-17 19:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function submitForm() {
+            let firstName = document.getElementById("firstName").value;
+            let lastName = document.getElementById("lastName").value;
+            let phoneNumber = document.getElementById("phoneNumber").value;
+            let email = document.getElementById("email").value;
+
+            let result = "First Name: " + firstName + "\n" +
+                         "Last Name: " + lastName + "\n" +
+                         "Phone Number: " + phoneNumber + "\n" +
+                         "Email ID: " + email;
+
+            alert(result);
+        }
